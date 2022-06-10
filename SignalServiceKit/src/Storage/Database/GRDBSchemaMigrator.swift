IbrahimUsmani118/Signal-iOS

commit 2477cd09e245b899e0a88c7c2d2d17f0deba3412

Store group avatars on disk

SignalServiceKit/src/Storage/Database/GRDBSchemaMigrator.swift
@@ -161,6 +161,7 @@ public class GRDBSchemaMigrator: NSObject {
         case dataMigration_cullInvalidIdentityKeySendingErrors
         case dataMigration_moveToThreadAssociatedData
         case dataMigration_senderKeyStoreKeyIdMigration
+        case dataMigration_migrateGroupAvatarsToDisk
     }
 
     public static let grdbSchemaVersionDefault: UInt = 0
@@ -1656,7 +1657,7 @@ public class GRDBSchemaMigrator: NSObject {
 
             TSGroupThread.anyEnumerate(transaction: transaction.asAnyWrite) { (thread: TSThread, _) in
                 guard let groupThread = thread as? TSGroupThread else { return }
-                guard let avatarData = groupThread.groupModel.groupAvatarData else { return }
+                guard let avatarData = groupThread.groupModel.legacyAvatarData else { return }
                 guard !TSGroupModel.isValidGroupAvatarData(avatarData) else { return }
 
                 var builder = groupThread.groupModel.asBuilder
@@ -1759,6 +1760,23 @@ public class GRDBSchemaMigrator: NSObject {
             defer { transaction.finalizeTransaction() }
 
             SenderKeyStore.performKeyIdMigration(transaction: transaction.asAnyWrite)
+        }
+
+        migrator.registerMigration(MigrationId.dataMigration_migrateGroupAvatarsToDisk.rawValue) { db in
+            let transaction = GRDBWriteTransaction(database: db)
+            defer { transaction.finalizeTransaction() }
+
+            let cursor = TSThread.grdbFetchCursor(
+                sql: "SELECT * FROM \(ThreadRecord.databaseTableName) WHERE \(threadColumn: .recordType) = \(SDSRecordType.groupThread.rawValue)",
+                transaction: transaction
+            )
+
+            while let thread = try cursor.next() as? TSGroupThread {
+                try autoreleasepool {
+                    try thread.groupModel.migrateLegacyAvatarDataToDisk()
+                    thread.anyUpsert(transaction: transaction.asAnyWrite)
+                }
+            }
         }
     }
 }

SignalServiceKit/src/groups/TSGroupModel.swift
@@ -176,7 +176,7 @@ public class TSGroupModelV2: TSGroupModel {
         result += "groupId: \(groupId.hexadecimalString),\n"
         result += "groupsVersion: \(groupsVersion),\n"
         result += "groupName: \(String(describing: groupName)),\n"
-        result += "groupAvatarData: \(String(describing: groupAvatarData?.hexadecimalString.prefix(32))),\n"
+        result += "avatarHash: \(String(describing: avatarHash)),\n"
         result += "membership: \(groupMembership.debugDescription),\n"
         result += "access: \(access.debugDescription),\n"
         result += "secretParamsData: \(secretParamsData.hexadecimalString.prefix(32)),\n"
@@ -263,5 +263,121 @@ public extension TSGroupModel {
             return []
         }
         return groupModelV2.droppedMembers
+    }
+}
+
+@objc
+public extension TSGroupModel {
+    private static let appSharedDataDirectory = URL(fileURLWithPath: OWSFileSystem.appSharedDataDirectoryPath())
+    static let avatarsDirectory = URL(fileURLWithPath: "GroupAvatars", isDirectory: true, relativeTo: appSharedDataDirectory)
+    @nonobjc
+    private static let avatarsCache = LRUCache<String, Data>(maxSize: 16, nseMaxSize: 0)
+
+    func migrateLegacyAvatarDataToDisk() throws {
+        guard let legacyAvatarData = legacyAvatarData else { return }
+        try persistAvatarData(legacyAvatarData)
+        self.legacyAvatarData = nil
+    }
+
+    func persistAvatarData(_ data: Data) throws {
+        guard !data.isEmpty else {
+            self.avatarHash = nil
+            return
+        }
+
+        guard Self.isValidGroupAvatarData(data) else {
+            throw OWSAssertionError("Invalid group avatar")
+        }
+
+        let hash = try Self.hash(forAvatarData: data)
+
+        OWSFileSystem.ensureDirectoryExists(Self.avatarsDirectory.path)
+
+        let filePath = Self.avatarFilePath(forHash: hash)
+        guard !OWSFileSystem.fileOrFolderExists(url: filePath) else {
+            // Avatar is already persisted.
+            self.avatarHash = hash
+            return
+        }
+
+        try data.write(to: Self.avatarFilePath(forHash: hash))
+        Self.avatarsCache.set(key: hash, value: data)
+
+        // Note: Old avatars are explicitly not cleaned up from the file
+        // system at this point, as multiple instances of a group model
+        // may be floating around referencing different versions of
+        // the avatar. We only purge old avatars from the file system
+        // when orphan data cleaner deems it safe to do so.
+
+        self.avatarHash = hash
+    }
+
+    class func hash(forAvatarData avatarData: Data) throws -> String {
+        guard let digest = Cryptography.computeSHA256Digest(avatarData) else {
+            throw OWSAssertionError("Unexpectedly failed to calculate avatar digest")
+        }
+
+        return digest.hexadecimalString
+    }
+
+    var avatarData: Data? {
+        if let avatarHash = avatarHash, let cachedData = Self.avatarsCache.object(forKey: avatarHash) {
+            return cachedData
+        }
+
+        guard let fileName = avatarFileName else { return nil }
+        let filePath = URL(fileURLWithPath: fileName, relativeTo: Self.avatarsDirectory)
+
+        let avatarData: Data
+        do {
+            avatarData = try Data(contentsOf: filePath)
+        } catch {
+            owsFailDebug("Failed to read group avatar data \(error)")
+            return nil
+        }
+
+        guard avatarData.ows_isValidImage else {
+            owsFailDebug("Invalid group avatar data.")
+            return nil
+        }
+
+        return avatarData
+    }
+
+    var avatarImage: UIImage? {
+        guard let avatarData = avatarData else {
+            return nil
+        }
+        return UIImage(data: avatarData)
+    }
+
+    var avatarFileName: String? {
+        guard let hash = avatarHash else { return nil }
+        return Self.avatarFileName(forHash: hash)
+    }
+
+    static func avatarFileName(forHash hash: String) -> String {
+        // All group avatars are PNGs, use the appropriate file extension.
+        return "\(hash).png"
+    }
+
+    static func avatarFilePath(forHash hash: String) -> URL {
+        URL(fileURLWithPath: avatarFileName(forHash: hash), relativeTo: avatarsDirectory)
+    }
+
+    class func allGroupAvatarFilePaths(transaction: SDSAnyReadTransaction) throws -> Set<String> {
+        let cursor = TSThread.grdbFetchCursor(
+            sql: "SELECT * FROM \(ThreadRecord.databaseTableName) WHERE \(threadColumn: .recordType) = \(SDSRecordType.groupThread.rawValue)",
+            transaction: transaction.unwrapGrdbRead
+        )
+
+        var filePaths = Set<String>()
+
+        while let thread = try cursor.next() as? TSGroupThread {
+            guard let avatarHash = thread.groupModel.avatarHash else { continue }
+            filePaths.insert(avatarFilePath(forHash: avatarHash).path)
+        }
+
+        return filePaths
     }
 }

Signal/src/ViewControllers/AppSettings/Privacy/BlockListViewController.m
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 #import "BlockListViewController.h"
@@ -134,7 +134,7 @@ NS_ASSUME_NONNULL_BEGIN
             @"BLOCK_LIST_BLOCKED_GROUPS_SECTION", @"Section header for groups that have been blocked");
 
         for (TSGroupModel *blockedGroup in blockedGroups) {
-            UIImage *_Nullable image = blockedGroup.groupAvatarImage;
+            UIImage *_Nullable image = blockedGroup.avatarImage;
             if (!image) {
                 image = [self.avatarBuilder avatarImageForGroupId:blockedGroup.groupId
                                                    diameterPoints:AvatarBuilder.standardAvatarSizePoints];

Signal/src/ViewControllers/ConversationView/CV/CVLoader.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 import Foundation
@@ -94,7 +94,7 @@ public class CVLoader: NSObject {
                         != (prevGroupModel as? TSGroupModelV2)?.descriptionText
                     return (groupModel.groupName != prevGroupModel.groupName ||
                                 groupDescriptionDidChange ||
-                                groupModel.groupAvatarData != prevGroupModel.groupAvatarData ||
+                                groupModel.avatarHash != prevGroupModel.avatarHash ||
                                 groupModel.groupMembership.fullMembers.count != prevGroupModel.groupMembership.fullMembers.count)
                 }()
 

Signal/src/ViewControllers/DebugUI/DebugUIStress.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 import Foundation
@@ -25,7 +25,7 @@ public extension DebugUIStress {
             return GroupManager.localCreateNewGroup(members: oldGroupThread.groupModel.groupMembers,
                                                     groupId: nil,
                                                     name: groupName,
-                                                    avatarData: oldGroupThread.groupModel.groupAvatarData,
+                                                    avatarData: oldGroupThread.groupModel.avatarData,
                                                     disappearingMessageToken: .disabledToken,
                                                     newGroupSeed: nil,
                                                     shouldSendMessage: false)
@@ -54,7 +54,7 @@ public extension DebugUIStress {
             let newGroupThread = try self.databaseStorage.write { (transaction: SDSAnyWriteTransaction) throws -> TSGroupThread in
                 let newGroupThread = try GroupManager.createGroupForTests(members: oldGroupThread.groupModel.groupMembers,
                                                                           name: groupName,
-                                                                          avatarData: oldGroupThread.groupModel.groupAvatarData,
+                                                                          avatarData: oldGroupThread.groupModel.avatarData,
                                                                           groupId: nil,
                                                                           groupsVersion: .V1,
                                                                           transaction: transaction)
@@ -105,7 +105,7 @@ public extension DebugUIStress {
             return GroupManager.localCreateNewGroup(members: members,
                                                     groupId: nil,
                                                     name: groupName,
-                                                    avatarData: oldGroupThread.groupModel.groupAvatarData,
+                                                    avatarData: oldGroupThread.groupModel.avatarData,
                                                     disappearingMessageToken: .disabledToken,
                                                     newGroupSeed: nil,
                                                     shouldSendMessage: false)

Signal/src/ViewControllers/ThreadSettings/GroupAttributesEditorHelper.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 import Foundation
@@ -81,7 +81,7 @@ class GroupAttributesEditorHelper: NSObject {
             groupId: groupModel.groupId,
             groupNameOriginal: groupModel.groupName,
             groupDescriptionOriginal: (groupModel as? TSGroupModelV2)?.descriptionText,
-            avatarOriginalData: groupModel.groupAvatarData,
+            avatarOriginalData: groupModel.avatarData,
             iconViewSize: iconViewSize,
             renderDefaultAvatarWhenCleared: renderDefaultAvatarWhenCleared
         )

Signal/src/util/Device Transfer/DeviceTransferService+Manifest.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 import Foundation
@@ -53,7 +53,8 @@ extension DeviceTransferService {
 
         // Attachments, Avatars, and Stickers
 
-        let foldersToTransfer = ["Attachments/", "ProfileAvatars/", "StickerManager/", "Wallpapers/", "Library/Sounds/", "AvatarHistory/"]
+        // TODO: Ideally, these paths would reference constants...
+        let foldersToTransfer = ["Attachments/", "ProfileAvatars/", "GroupAvatars/", "StickerManager/", "Wallpapers/", "Library/Sounds/", "AvatarHistory/"]
         let filesToTransfer = try foldersToTransfer.flatMap { folder -> [String] in
             let url = URL(fileURLWithPath: folder, relativeTo: DeviceTransferService.appSharedDataDirectory)
             return try OWSFileSystem.recursiveFilesInDirectory(url.path)

SignalMessaging/contacts/OWSContactsManager.swift
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
+//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
 //
 
 import Foundation
@@ -144,7 +144,7 @@ fileprivate extension OWSContactsManager {
         if cache.contains(groupThread: groupThread) {
             return false
         }
-        if nil == groupThread.groupModel.groupAvatarData {
+        if nil == groupThread.groupModel.avatarHash {
             // DO NOT add to the cache.
             return false
         }

commit 0e35dd968d1f92ed34748e492a0cad15fe23f477

Clear invalid legacy avatars, instead of crashing
A migration (introduced in 2477cd09e245b899e0a88c7c2d2d17f0deba3412)
tries to save legacy avatars to disk. However, if that legacy avatar
data is invalid, we'd crash on launch.

A user encountered this.

Our possible solutions:

1. If legacy avatar data is invalid, just drop it.
2. If legacy avatar data is invalid, try to repair it. (For example, if
   it's too large, try to resize it.) This might not fix the user's
   issue—unfortunately, we don't know why the avatar was invalid.
3. Loosen our definition of a valid avatar. (For example, increase the
   maximum size.) This also might not fix things—we don't know how the
   avatar was invalid.

I went with the first solution.

This was difficult to test. While developing, I made a temporary button
that (1) updated a group, giving it an empty legacy avatar buffer (2)
tried to migrate it.

(Thanks to f7f9c24ee4e541985db537325ce795259094bf95 for making this easy
to diagnose!)

SignalServiceKit/src/Storage/Database/GRDBSchemaMigrator.swift
@@ -2035,7 +2035,7 @@ public class GRDBSchemaMigrator: NSObject {
 
                 while let thread = try threadCursor.next() as? TSGroupThread {
                     try autoreleasepool {
-                        try thread.groupModel.migrateLegacyAvatarDataToDisk()
+                        try thread.groupModel.attemptToMigrateLegacyAvatarDataToDisk()
                         thread.anyUpsert(transaction: transaction.asAnyWrite)
                         GRDBFullTextSearchFinder.modelWasUpdated(model: thread, transaction: transaction)
                     }

SignalServiceKit/src/groups/TSGroupModel.swift
@@ -273,8 +273,19 @@ public extension TSGroupModel {
     @nonobjc
     private static let avatarsCache = LRUCache<String, Data>(maxSize: 16, nseMaxSize: 0)
 
-    func migrateLegacyAvatarDataToDisk() throws {
-        guard let legacyAvatarData = legacyAvatarData else { return }
+    func attemptToMigrateLegacyAvatarDataToDisk() throws {
+        guard let legacyAvatarData = legacyAvatarData, !legacyAvatarData.isEmpty else {
+            self.legacyAvatarData = nil
+            return
+        }
+
+        guard Self.isValidGroupAvatarData(legacyAvatarData) else {
+            owsFailDebug("Invalid legacy avatar data. Removing it completely")
+            self.avatarHash = nil
+            self.legacyAvatarData = nil
+            return
+        }
+
         try persistAvatarData(legacyAvatarData)
         self.legacyAvatarData = nil
     }