IbrahimUsmani118/Signal-iOS

commit 411d5c6d2638e25970a6424579824dc7f4033fcf

Combine interactive sheet handle code, fix layout bugs
This makes two changes to `InteractiveSheetViewController` and its
subclasses:

1. Consolidate the handle (the little grabber at the top) code.
   Previously, if the handle was visibly inside the sheet (instead of
   above it), it was a bespoke view for each sheet. Now, they all use
   the same handle and the same code.
2. Some sheets didn't account for the handle, which meant that they were
   cut off a bit at the top. This fixes that in
   `InteractiveSheetViewController`, and therefore all subclasses.

This might mean that some sheets look _slightly_ different (or fixed!).

Signal/src/ViewControllers/ThreadSettings/MemberActionSheet.swift
@@ -11,7 +11,6 @@ class MemberActionSheet: InteractiveSheetViewController {
     let tableViewController = OWSTableViewController2()
 
     private var groupViewHelper: GroupViewHelper?
-    private let handleContainer = UIView()
 
     var avatarView: PrimaryImageView?
     var thread: TSThread { threadViewModel.threadRecord }
@@ -19,7 +18,7 @@ class MemberActionSheet: InteractiveSheetViewController {
     let address: SignalServiceAddress
 
     override var interactiveScrollViews: [UIScrollView] { [tableViewController.tableView] }
-    override var renderExternalHandle: Bool { false }
+    override var sheetBackgroundColor: UIColor {  tableViewController.tableBackgroundColor }
 
     var contentSizeHeight: CGFloat {
         tableViewController.tableView.contentSize.height + tableViewController.tableView.adjustedContentInset.totalHeight
@@ -89,21 +88,6 @@ class MemberActionSheet: InteractiveSheetViewController {
         contentView.addSubview(tableViewController.view)
         tableViewController.view.autoPinEdgesToSuperviewEdges()
 
-        // We add the handle directly to the content view,
-        // so that it doesn't scroll with the table.
-        handleContainer.backgroundColor = tableViewController.tableBackgroundColor
-        contentView.addSubview(handleContainer)
-        handleContainer.autoPinWidthToSuperview()
-        handleContainer.autoPinEdge(toSuperviewEdge: .top)
-
-        let handle = UIView()
-        handle.backgroundColor = tableViewController.separatorColor
-        handle.autoSetDimensions(to: CGSize(width: 36, height: 5))
-        handle.layer.cornerRadius = 5 / 2
-        handleContainer.addSubview(handle)
-        handle.autoPinHeightToSuperview(withMargin: 12)
-        handle.autoHCenterInSuperview()
-
         updateViewState()
     }
 
@@ -128,7 +112,6 @@ class MemberActionSheet: InteractiveSheetViewController {
 
     override func themeDidChange() {
         super.themeDidChange()
-        handleContainer.backgroundColor = tableViewController.tableBackgroundColor
         updateTableContents()
     }
 

Signal/src/Calls/UserInterface/Group/GroupCallMemberSheet.swift
@@ -8,8 +8,14 @@ import SignalRingRTC
 @objc
 class GroupCallMemberSheet: InteractiveSheetViewController {
     override var interactiveScrollViews: [UIScrollView] { [tableView] }
+    override var handlePosition: HandlePosition { .outside }
+
     let tableView = UITableView(frame: .zero, style: .grouped)
     let call: SignalCall
+
+    override var sheetBackgroundColor: UIColor {
+        UIAccessibility.isReduceTransparencyEnabled ? .ows_blackAlpha80 : .ows_blackAlpha40
+    }
 
     init(call: SignalCall) {
         self.call = call
@@ -28,13 +34,10 @@ class GroupCallMemberSheet: InteractiveSheetViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
 
-        if UIAccessibility.isReduceTransparencyEnabled {
-            contentView.backgroundColor = .ows_blackAlpha80
-        } else {
+        if !UIAccessibility.isReduceTransparencyEnabled {
             let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
             contentView.addSubview(blurEffectView)
             blurEffectView.autoPinEdgesToSuperviewEdges()
-            contentView.backgroundColor = .ows_blackAlpha40
         }
 
         tableView.dataSource = self

Signal/src/ViewControllers/AppSettings/BadgeDetailsSheet.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class BadgeDetailsSheet: InteractiveSheetViewController {
     override var interactiveScrollViews: [UIScrollView] { [tableViewController.tableView] }
-    override var renderExternalHandle: Bool { false }
+    override var sheetBackgroundColor: UIColor { tableViewController.tableBackgroundColor }
 
     var contentSizeHeight: CGFloat {
         tableViewController.tableView.contentSize.height + tableViewController.tableView.adjustedContentInset.totalHeight
@@ -22,7 +22,6 @@ class BadgeDetailsSheet: InteractiveSheetViewController {
     }
 
     private let tableViewController = OWSTableViewController2()
-    private let handleContainer = UIView()
 
     public enum Owner: Equatable {
         // TODO: Eventually we won't need a short name for self, the server will provide copy for us.
@@ -70,7 +69,6 @@ class BadgeDetailsSheet: InteractiveSheetViewController {
 
     override func themeDidChange() {
         super.themeDidChange()
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
         updateTableContents()
     }
 
@@ -80,21 +78,6 @@ class BadgeDetailsSheet: InteractiveSheetViewController {
 
         contentView.addSubview(tableViewController.view)
         tableViewController.view.autoPinEdgesToSuperviewEdges()
-
-        // We add the handle directly to the content view,
-        // so that it doesn't scroll with the table.
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
-        contentView.addSubview(handleContainer)
-        handleContainer.autoPinWidthToSuperview()
-        handleContainer.autoPinEdge(toSuperviewEdge: .top)
-
-        let handle = UIView()
-        handle.backgroundColor = tableViewController.separatorColor
-        handle.autoSetDimensions(to: CGSize(width: 36, height: 5))
-        handle.layer.cornerRadius = 5 / 2
-        handleContainer.addSubview(handle)
-        handle.autoPinHeightToSuperview(withMargin: 12)
-        handle.autoHCenterInSuperview()
 
         updateViewState()
     }

Signal/src/ViewControllers/AppSettings/BadgeExpirationSheet.swift
@@ -13,7 +13,7 @@ protocol BadgeExpirationSheetDelegate: AnyObject {
 
 class BadgeExpirationSheet: InteractiveSheetViewController {
     override var interactiveScrollViews: [UIScrollView] { [tableViewController.tableView] }
-    override var renderExternalHandle: Bool { false }
+    override var sheetBackgroundColor: UIColor { tableViewController.tableBackgroundColor }
     private var shouldMakeVisibleAndPrimary = false
     public weak var delegate: BadgeExpirationSheetDelegate?
 
@@ -29,7 +29,6 @@ class BadgeExpirationSheet: InteractiveSheetViewController {
     }
 
     private let tableViewController = OWSTableViewController2()
-    private let handleContainer = UIView()
 
     private let badge: ProfileBadge
 
@@ -63,21 +62,6 @@ class BadgeExpirationSheet: InteractiveSheetViewController {
         contentView.addSubview(tableViewController.view)
         tableViewController.view.autoPinEdgesToSuperviewEdges()
 
-        // We add the handle directly to the content view,
-        // so that it doesn't scroll with the table.
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
-        contentView.addSubview(handleContainer)
-        handleContainer.autoPinWidthToSuperview()
-        handleContainer.autoPinEdge(toSuperviewEdge: .top)
-
-        let handle = UIView()
-        handle.backgroundColor = tableViewController.separatorColor
-        handle.autoSetDimensions(to: CGSize(width: 36, height: 5))
-        handle.layer.cornerRadius = 5 / 2
-        handleContainer.addSubview(handle)
-        handle.autoPinHeightToSuperview(withMargin: 12)
-        handle.autoHCenterInSuperview()
-
         updateViewState()
     }
 
@@ -97,7 +81,6 @@ class BadgeExpirationSheet: InteractiveSheetViewController {
 
     override func themeDidChange() {
         super.themeDidChange()
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
         updateTableContents()
     }
 

Signal/src/ViewControllers/AppSettings/BadgeThanksSheet.swift
@@ -8,7 +8,7 @@ import SignalUI
 
 class BadgeThanksSheet: InteractiveSheetViewController {
     override var interactiveScrollViews: [UIScrollView] { [tableViewController.tableView] }
-    override var renderExternalHandle: Bool { false }
+    override var sheetBackgroundColor: UIColor { tableViewController.tableBackgroundColor }
     private var shouldMakeVisibleAndPrimary = false
 
     var contentSizeHeight: CGFloat {
@@ -22,7 +22,6 @@ class BadgeThanksSheet: InteractiveSheetViewController {
     }
 
     private let tableViewController = OWSTableViewController2()
-    private let handleContainer = UIView()
 
     private let badge: ProfileBadge
     private var isBoost: Bool { BoostBadgeIds.contains(badge.id) }
@@ -136,21 +135,6 @@ class BadgeThanksSheet: InteractiveSheetViewController {
         contentView.addSubview(tableViewController.view)
         tableViewController.view.autoPinEdgesToSuperviewEdges()
 
-        // We add the handle directly to the content view,
-        // so that it doesn't scroll with the table.
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
-        contentView.addSubview(handleContainer)
-        handleContainer.autoPinWidthToSuperview()
-        handleContainer.autoPinEdge(toSuperviewEdge: .top)
-
-        let handle = UIView()
-        handle.backgroundColor = tableViewController.separatorColor
-        handle.autoSetDimensions(to: CGSize(width: 36, height: 5))
-        handle.layer.cornerRadius = 5 / 2
-        handleContainer.addSubview(handle)
-        handle.autoPinHeightToSuperview(withMargin: 12)
-        handle.autoHCenterInSuperview()
-
         if isBoost && hasAnySustainerBadge {
             shouldMakeVisibleAndPrimary = false
         } else {
@@ -176,7 +160,6 @@ class BadgeThanksSheet: InteractiveSheetViewController {
 
     override func themeDidChange() {
         super.themeDidChange()
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
         updateTableContents()
     }
 

Signal/src/ViewControllers/AppSettings/BoostViewController.swift
@@ -12,42 +12,18 @@ import SignalUI
 
 class BoostSheetView: InteractiveSheetViewController {
     let boostVC = BoostViewController()
-    let handleContainer = UIView()
     override var interactiveScrollViews: [UIScrollView] { [boostVC.tableView] }
     override var minHeight: CGFloat { min(680, CurrentAppContext().frame.height - (view.safeAreaInsets.top + 32)) }
     override var maximizedHeight: CGFloat { minHeight }
-    override var renderExternalHandle: Bool { false }
 
     // MARK: -
 
     override public func viewDidLoad() {
         super.viewDidLoad()
 
-        // We add the handle directly to the content view,
-        // so that it doesn't scroll with the table.
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
-        contentView.addSubview(handleContainer)
-        handleContainer.autoPinWidthToSuperview()
-        handleContainer.autoPinEdge(toSuperviewEdge: .top)
-
-        let handle = UIView()
-        handle.backgroundColor = boostVC.separatorColor
-        handle.autoSetDimensions(to: CGSize(width: 36, height: 5))
-        handle.layer.cornerRadius = 5 / 2
-        handleContainer.addSubview(handle)
-        handle.autoPinHeightToSuperview(withMargin: 12)
-        handle.autoHCenterInSuperview()
-
         contentView.addSubview(boostVC.view)
-        boostVC.view.autoPinWidthToSuperview()
-        boostVC.view.autoPinEdge(toSuperviewEdge: .bottom)
-        boostVC.view.autoPinEdge(.top, to: .bottom, of: handleContainer)
+        boostVC.view.autoPinEdgesToSuperviewEdges()
         addChild(boostVC)
-    }
-
-    override func themeDidChange() {
-        super.themeDidChange()
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
     }
 }
 

Signal/src/ViewControllers/AppSettings/DonationViewsUtil.swift
@@ -49,32 +49,15 @@ public class ProfileBadgeLookup {
 public class SubscriptionReadMoreSheet: InteractiveSheetViewController {
     let contentScrollView = UIScrollView()
     let stackView = UIStackView()
-    let handleContainer = UIView()
     public override var interactiveScrollViews: [UIScrollView] { [contentScrollView] }
     public override var minHeight: CGFloat { min(740, CurrentAppContext().frame.height - (view.safeAreaInsets.top + 32)) }
     override var maximizedHeight: CGFloat { minHeight }
-    public override var renderExternalHandle: Bool { false }
+    override var sheetBackgroundColor: UIColor { Theme.tableView2PresentedBackgroundColor }
 
     // MARK: -
 
     override public func viewDidLoad() {
         super.viewDidLoad()
-        contentView.backgroundColor = Theme.tableView2PresentedBackgroundColor
-
-        // We add the handle directly to the content view,
-        // so that it doesn't scroll with the table.
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
-        contentView.addSubview(handleContainer)
-        handleContainer.autoPinWidthToSuperview()
-        handleContainer.autoPinEdge(toSuperviewEdge: .top)
-
-        let handle = UIView()
-        handle.backgroundColor = Theme.tableView2PresentedSeparatorColor
-        handle.autoSetDimensions(to: CGSize(width: 36, height: 5))
-        handle.layer.cornerRadius = 5 / 2
-        handleContainer.addSubview(handle)
-        handle.autoPinHeightToSuperview(withMargin: 12)
-        handle.autoHCenterInSuperview()
 
         contentView.addSubview(contentScrollView)
 
@@ -86,19 +69,10 @@ public class SubscriptionReadMoreSheet: InteractiveSheetViewController {
         // Pin to the scroll view's viewport, not to its scrollable area
         stackView.autoPinWidth(toWidthOf: contentScrollView)
 
-        contentScrollView.autoPinWidthToSuperview()
-        contentScrollView.autoPinEdge(toSuperviewEdge: .bottom)
-        contentScrollView.autoPinEdge(.top, to: .bottom, of: handleContainer)
+        contentScrollView.autoPinEdgesToSuperviewEdges()
         contentScrollView.alwaysBounceVertical = true
 
         buildContents()
-    }
-
-    public override func themeDidChange() {
-        super.themeDidChange()
-        handleContainer.backgroundColor = Theme.tableView2PresentedBackgroundColor
-        contentView.backgroundColor = Theme.tableView2PresentedBackgroundColor
-
     }
 
     private func buildContents() {

Signal/src/ViewControllers/ConversationView/BlockingAnnouncementOnlyView.swift
@@ -116,8 +116,6 @@ class MessageUserSubsetSheet: InteractiveSheetViewController {
     override var interactiveScrollViews: [UIScrollView] { [tableViewController.tableView] }
     private let tableViewController = OWSTableViewController2()
     private let addresses: [SignalServiceAddress]
-    override var renderExternalHandle: Bool { false }
-    private let handleContainer = UIView()
 
     var contentSizeHeight: CGFloat {
         tableViewController.tableView.contentSize.height + tableViewController.tableView.adjustedContentInset.totalHeight
@@ -152,7 +150,6 @@ class MessageUserSubsetSheet: InteractiveSheetViewController {
 
     override func themeDidChange() {
         super.themeDidChange()
-        handleContainer.backgroundColor = tableViewController.tableBackgroundColor
         updateTableContents()
     }
 
@@ -168,21 +165,6 @@ class MessageUserSubsetSheet: InteractiveSheetViewController {
                            forCellReuseIdentifier: ContactTableViewCell.reuseIdentifier)
         contentView.addSubview(tableViewController.view)
         tableViewController.view.autoPinEdgesToSuperviewEdges()
-
-        // We add the handle directly to the content view,
-        // so that it doesn't scroll with the table.
-        handleContainer.backgroundColor = tableViewController.tableBackgroundColor
-        contentView.addSubview(handleContainer)
-        handleContainer.autoPinWidthToSuperview()
-        handleContainer.autoPinEdge(toSuperviewEdge: .top)
-
-        let handle = UIView()
-        handle.backgroundColor = tableViewController.separatorColor
-        handle.autoSetDimensions(to: CGSize(width: 36, height: 5))
-        handle.layer.cornerRadius = 5 / 2
-        handleContainer.addSubview(handle)
-        handle.autoPinHeightToSuperview(withMargin: 12)
-        handle.autoHCenterInSuperview()
 
         updateViewState()
     }
@@ -208,11 +190,6 @@ class MessageUserSubsetSheet: InteractiveSheetViewController {
 
     private func updateTableContents() {
         let contents = OWSTableContents()
-
-        // Leave space at the top for the handle
-        let handleSection = OWSTableSection()
-        handleSection.customHeaderHeight = 25
-        contents.addSection(handleSection)
 
         let section = OWSTableSection()
         let header = NSLocalizedString("GROUPS_ANNOUNCEMENT_ONLY_CONTACT_ADMIN",

commit f0c908df53f4830b26eaaabb892e2b1fb3d43ee0

Fix small visual bugs in group member sheet
This fixes two small regressions introduced in
411d5c6d2638e25970a6424579824dc7f4033fcf:

1. There's a bit too much spacing at the top.

2. In dark mode, the grabber had the wrong background color. (As you can
   see in the code, my solution here is a temporary workaround before we
   do a larger refactor.)

Signal/src/ViewControllers/ThreadSettings/MemberActionSheet.swift
@@ -18,7 +18,18 @@ class MemberActionSheet: InteractiveSheetViewController {
     let address: SignalServiceAddress
 
     override var interactiveScrollViews: [UIScrollView] { [tableViewController.tableView] }
-    override var sheetBackgroundColor: UIColor {  tableViewController.tableBackgroundColor }
+
+    override var sheetBackgroundColor: UIColor {
+        // We can't use `tableViewController.tableBackgroundColor` directly because it changes
+        // depending on whether it's inside a view controller. When this variable is first used,
+        // `tableViewController` isn't a child yet, so the color will be different.
+        //
+        // In the long term, we should consider a larger refactor, likely by turning this sheet's
+        // contents into a "regular" view.
+        //
+        // See IOS-2468 for more details.
+        OWSTableViewController2.tableBackgroundColor(isUsingPresentedStyle: true)
+    }
 
     var contentSizeHeight: CGFloat {
         tableViewController.tableView.contentSize.height + tableViewController.tableView.adjustedContentInset.totalHeight
@@ -128,10 +139,9 @@ class MemberActionSheet: InteractiveSheetViewController {
         let contents = OWSTableContents()
         defer { tableViewController.setContents(contents, shouldReload: shouldReload) }
 
-        // Leave space at the top for the handle
-        let handleSection = OWSTableSection()
-        handleSection.customHeaderHeight = 50
-        contents.addSection(handleSection)
+        let topSpacerSection = OWSTableSection()
+        topSpacerSection.customHeaderHeight = 12
+        contents.addSection(topSpacerSection)
 
         let section = OWSTableSection()
         contents.addSection(section)