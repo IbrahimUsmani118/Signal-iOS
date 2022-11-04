IbrahimUsmani118/Signal-iOS

commit 622b5b2db11b8aa10bdf5ca3bc2eb9e6c6c0b844

"Add to Another Group": show archived groups, order by recency
Previously, archived groups didn't show up in the "Add to Another Group"
view. Now, they do!

This also orders groups entirely by recency.

This view used `ThreadViewHelper`, which (1) omitted archived groups
(2) had a bunch of dormant database observation code. I removed this
class and replaced it with some inline code that fetches groups.

Signal/src/ViewControllers/ThreadSettings/AddToGroupViewController.swift
@@ -8,14 +8,6 @@ import Foundation
 public class AddToGroupViewController: OWSTableViewController2 {
 
     private let address: SignalServiceAddress
-    private let collation = UILocalizedIndexedCollation.current()
-    private let maxRecentGroups = 7
-
-    private lazy var threadViewHelper: ThreadViewHelper = {
-        let threadViewHelper = ThreadViewHelper()
-        threadViewHelper.delegate = self
-        return threadViewHelper
-    }()
 
     init(address: SignalServiceAddress) {
         self.address = address
@@ -47,79 +39,48 @@ public class AddToGroupViewController: OWSTableViewController2 {
 
     private var groupThreads = [TSGroupThread]() {
         didSet {
-            AssertIsOnMainThread()
-            updateTableContents()
+            DispatchQueue.main.async { [weak self] in
+                self?.updateTableContents()
+            }
         }
     }
+
     private func updateGroupThreadsAsync() {
-        // make sure threadViewHelper is prepared on the main thread
-        _ = threadViewHelper
-        DispatchQueue.sharedUserInitiated.async { self.updateGroupThreads() }
-    }
-    private func updateGroupThreads() {
-        owsAssertDebug(!Thread.isMainThread)
-
-        let groupThreads = databaseStorage.read { transaction in
-            return self.threadViewHelper.threads.filter { thread -> Bool in
-                guard let groupThread = thread as? TSGroupThread else { return false }
-                let threadViewModel = ThreadViewModel(thread: groupThread,
-                                                      forChatList: false,
-                                                      transaction: transaction)
-                let groupViewHelper = GroupViewHelper(threadViewModel: threadViewModel)
-                return groupViewHelper.canEditConversationMembership
-            } as? [TSGroupThread] ?? []
+        DispatchQueue.sharedUserInitiated.async { [weak self] in
+            self?.groupThreads = Self.fetchGroupThreads()
         }
+    }
 
-        DispatchQueue.main.async {
-            self.groupThreads = groupThreads
+    private class func fetchGroupThreads() -> [TSGroupThread] {
+        databaseStorage.read { transaction in
+            var result = [TSGroupThread]()
+
+            do {
+                try AnyThreadFinder().enumerateGroupThreads(transaction: transaction) { thread in
+                    guard thread.groupModel.groupsVersion == .V2 else { return }
+
+                    let threadViewModel = ThreadViewModel(
+                        thread: thread,
+                        forChatList: false,
+                        transaction: transaction
+                    )
+                    let groupViewHelper = GroupViewHelper(threadViewModel: threadViewModel)
+                    guard groupViewHelper.canEditConversationMembership else { return }
+
+                    result.append(thread)
+                }
+            } catch {
+                owsFailDebug("Failed to fetch group threads: \(error). Returning an empty array")
+            }
+
+            return result
         }
     }
 
     private func updateTableContents() {
         AssertIsOnMainThread()
-
-        let contents = OWSTableContents()
-
-        let recentGroups = groupThreads.count > maxRecentGroups ? Array(groupThreads[0..<maxRecentGroups]) : groupThreads
-
-        let recentsSection = OWSTableSection()
-        recentsSection.headerTitle = NSLocalizedString("ADD_TO_GROUP_RECENTS_TITLE",
-                                                       comment: "The title for the 'add to group' view's recents section")
-        recentsSection.add(items: recentGroups.map(item(forGroupThread:)))
-        contents.addSection(recentsSection)
-
-        if let additionalGroups = groupThreads.count > maxRecentGroups ? Array(groupThreads[maxRecentGroups..<groupThreads.count]) : nil {
-            let collatedGroups = additionalGroups.reduce(into: [Int: [TSGroupThread]]()) { result, group in
-                let section = collation.section(for: group, collationStringSelector: #selector(getter: TSGroupThread.groupNameOrDefault))
-                var sectionGroups = result[section] ?? []
-                sectionGroups.append(group)
-                result[section] = sectionGroups
-            }
-
-            for (section, title) in collation.sectionTitles.enumerated() {
-                guard let sectionGroups = collatedGroups[section] else { continue }
-
-                let section = OWSTableSection()
-                section.headerTitle = title
-                section.add(items: sectionGroups
-                        .sorted { $0.groupNameOrDefault.localizedCaseInsensitiveCompare($1.groupNameOrDefault) == .orderedAscending }
-                        .map(item(forGroupThread:))
-                )
-
-                contents.addSection(section)
-            }
-
-            let visibleTitles: [String] = collation.sectionTitles.enumerated().compactMap { (index, title) in
-                guard collatedGroups[index] != nil else { return nil }
-                return title
-            }
-
-            contents.sectionForSectionIndexTitleBlock = { $1 + 1 }
-            contents.sectionIndexTitlesForTableViewBlock = { visibleTitles }
-
-        }
-
-        self.contents = contents
+        let groupsSection = OWSTableSection(items: groupThreads.map(item(forGroupThread:)))
+        self.contents = OWSTableContents(sections: [groupsSection])
     }
 
     // MARK: Helpers
@@ -260,13 +221,5 @@ public class AddToGroupViewController: OWSTableViewController2 {
                 self?.didSelectGroup(groupThread)
             }
         )
-    }
-}
-
-// MARK: -
-
-extension AddToGroupViewController: ThreadViewHelperDelegate {
-    public func threadListDidChange() {
-        updateGroupThreadsAsync()
     }
 }

Signal.xcodeproj/project.pbxproj
@@ -123,7 +123,6 @@
 		3402AA75271D9E180084CBAE /* ConversationAvatarView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A95543271B510400B05242 /* ConversationAvatarView.swift */; };
 		3402AA76271D9E180084CBAE /* ImageEditorStrokeItem.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A95559271B510400B05242 /* ImageEditorStrokeItem.swift */; };
 		3402AA77271D9E180084CBAE /* LinkPreviewState.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A95516271B510400B05242 /* LinkPreviewState.swift */; };
-		3402AA78271D9E180084CBAE /* ThreadViewHelper.m in Sources */ = {isa = PBXBuildFile; fileRef = 34A95518271B510400B05242 /* ThreadViewHelper.m */; };
 		3402AA79271D9E180084CBAE /* PermissiveGestureRecognizer.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A95557271B510400B05242 /* PermissiveGestureRecognizer.swift */; };
 		3402AA7A271D9E180084CBAE /* CVUtils.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A95524271B510400B05242 /* CVUtils.swift */; };
 		3402AA7B271D9E180084CBAE /* CustomKeyboard.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A9553A271B510400B05242 /* CustomKeyboard.swift */; };
@@ -164,7 +163,6 @@
 		3402AAA1271D9E180084CBAE /* OWSBubbleView.h in Headers */ = {isa = PBXBuildFile; fileRef = 34A95534271B510400B05242 /* OWSBubbleView.h */; settings = {ATTRIBUTES = (Public, ); }; };
 		3402AAA2271D9E180084CBAE /* OWSTextView.m in Sources */ = {isa = PBXBuildFile; fileRef = 34A9550D271B510400B05242 /* OWSTextView.m */; };
 		3402AAA3271D9E180084CBAE /* DirectionalPanGestureRecognizer.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A95526271B510400B05242 /* DirectionalPanGestureRecognizer.swift */; };
-		3402AAA5271D9E180084CBAE /* ThreadViewHelper.h in Headers */ = {isa = PBXBuildFile; fileRef = 34A95547271B510400B05242 /* ThreadViewHelper.h */; settings = {ATTRIBUTES = (Public, ); }; };
 		3402AAA6271D9E180084CBAE /* MentionPicker.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A9552C271B510400B05242 /* MentionPicker.swift */; };
 		3402AAA7271D9E180084CBAE /* AvatarImageView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A95545271B510400B05242 /* AvatarImageView.swift */; };
 		3402AAA8271D9E180084CBAE /* MentionTextView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 34A9552D271B510400B05242 /* MentionTextView.swift */; };
@@ -2541,7 +2539,6 @@
 		34A95515271B510400B05242 /* LinkPreviewView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = LinkPreviewView.swift; sourceTree = "<group>"; };
 		34A95516271B510400B05242 /* LinkPreviewState.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = LinkPreviewState.swift; sourceTree = "<group>"; };
 		34A95517271B510400B05242 /* GroupLinkPreview.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = GroupLinkPreview.swift; sourceTree = "<group>"; };
-		34A95518271B510400B05242 /* ThreadViewHelper.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = ThreadViewHelper.m; sourceTree = "<group>"; };
 		34A95519271B510400B05242 /* VideoPlayerView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = VideoPlayerView.swift; sourceTree = "<group>"; };
 		34A9551B271B510400B05242 /* VideoEditorModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = VideoEditorModel.swift; sourceTree = "<group>"; };
 		34A9551C271B510400B05242 /* VideoEditorView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = VideoEditorView.swift; sourceTree = "<group>"; };
@@ -2580,7 +2577,6 @@
 		34A95544271B510400B05242 /* OWSNavigationBar.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = OWSNavigationBar.swift; sourceTree = "<group>"; };
 		34A95545271B510400B05242 /* AvatarImageView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AvatarImageView.swift; sourceTree = "<group>"; };
 		34A95546271B510400B05242 /* GradientView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = GradientView.swift; sourceTree = "<group>"; };
-		34A95547271B510400B05242 /* ThreadViewHelper.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = ThreadViewHelper.h; sourceTree = "<group>"; };
 		34A95548271B510400B05242 /* GalleryRailView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = GalleryRailView.swift; sourceTree = "<group>"; };
 		34A9554C271B510400B05242 /* ViewOnceTooltip.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ViewOnceTooltip.swift; sourceTree = "<group>"; };
 		34A9554D271B510400B05242 /* TooltipView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = TooltipView.swift; sourceTree = "<group>"; };
@@ -5215,8 +5211,6 @@
 				34A95528271B510400B05242 /* TextFieldWithPlaceholder.swift */,
 				34A9553E271B510400B05242 /* TextViewWithPlaceholder.swift */,
 				34A95537271B510400B05242 /* ThemeHeaderView.swift */,
-				34A95547271B510400B05242 /* ThreadViewHelper.h */,
-				34A95518271B510400B05242 /* ThreadViewHelper.m */,
 				34A95564271B510400B05242 /* Toast.swift */,
 				34A95519271B510400B05242 /* VideoPlayerView.swift */,
 			);
@@ -8220,7 +8214,6 @@
 				34A954CF271B4D0700B05242 /* SignalUI-Prefix.pch in Headers */,
 				34A954BA271A471300B05242 /* SignalUI.h in Headers */,
 				34A954DB271B4F3E00B05242 /* Theme.h in Headers */,
-				3402AAA5271D9E180084CBAE /* ThreadViewHelper.h in Headers */,
 				34A954FF271B4FB000B05242 /* UIFont+OWS.h in Headers */,
 				3402A9F4271D9A060084CBAE /* UIUtil.h in Headers */,
 				3402A9E9271D97090084CBAE /* UIView+SignalUI.h in Headers */,
@@ -9746,7 +9739,6 @@
 				34A954E1271B4F3E00B05242 /* Theme.m in Sources */,
 				3402AA8F271D9E180084CBAE /* ThemeHeaderView.swift in Sources */,
 				3402AA62271D9DEC0084CBAE /* ThreadUtil+SignalUI.swift in Sources */,
-				3402AA78271D9E180084CBAE /* ThreadViewHelper.m in Sources */,
 				3402AA65271D9DEC0084CBAE /* ThreadViewModel.swift in Sources */,
 				3402AA96271D9E180084CBAE /* Toast.swift in Sources */,
 				3402AA9B271D9E180084CBAE /* TooltipView.swift in Sources */,

Signal/translations/en.lproj/Localizable.strings
@@ -73,9 +73,6 @@
 /* A toast on the 'add to group' view indicating the user is already a member. Embeds {contact name} and {group name} */
 "ADD_TO_GROUP_ALREADY_MEMBER_TOAST_FORMAT" = "%@ is already a member of %@";
 
-/* The title for the 'add to group' view's recents section */
-"ADD_TO_GROUP_RECENTS_TITLE" = "Recent Chats";
-
 /* A toast on the 'add to group' view indicating the user was added. Embeds {contact name} and {group name} */
 "ADD_TO_GROUP_SUCCESS_TOAST_FORMAT" = "%@ was added to %@";
 

SignalServiceKit/src/Storage/Database/Records/ThreadFinder.swift
@@ -25,6 +25,14 @@ public class AnyThreadFinder: NSObject, ThreadFinder {
 
     let grdbAdapter: GRDBThreadFinder = GRDBThreadFinder()
 
+    /// Enumerates group threads in "last interaction" order.
+    public func enumerateGroupThreads(transaction: SDSAnyReadTransaction, block: @escaping (TSGroupThread) -> Void) throws {
+        switch transaction.readTransaction {
+        case .grdbRead(let grdb):
+            try grdbAdapter.enumerateGroupThreads(transaction: grdb, block: block)
+        }
+    }
+
     public func visibleThreadCount(isArchived: Bool, transaction: SDSAnyReadTransaction) throws -> UInt {
         switch transaction.readTransaction {
         case .grdbRead(let grdb):
@@ -112,6 +120,22 @@ public class GRDBThreadFinder: NSObject, ThreadFinder {
     public typealias ReadTransaction = GRDBReadTransaction
 
     static let cn = ThreadRecord.columnName
+
+    /// Enumerates group threads in "last interaction" order.
+    public func enumerateGroupThreads(transaction: GRDBReadTransaction, block: @escaping (TSGroupThread) -> Void) throws {
+        let sql = """
+            SELECT *
+            FROM \(ThreadRecord.databaseTableName)
+            WHERE \(threadColumn: .groupModel) IS NOT NULL
+            ORDER BY \(threadColumn: .lastInteractionRowId) DESC
+        """
+
+        try ThreadRecord.fetchCursor(transaction.database, sql: sql).forEach { threadRecord in
+            let thread = try TSThread.fromRecord(threadRecord)
+            guard let groupThread = thread as? TSGroupThread else { return }
+            block(groupThread)
+        }
+    }
 
     public func visibleThreadCount(isArchived: Bool, transaction: GRDBReadTransaction) throws -> UInt {
         let sql = """

SignalUI/SignalUI.h
@@ -31,7 +31,6 @@ FOUNDATION_EXPORT const unsigned char SignalUIVersionString[];
 #import <SignalUI/RecipientPickerViewController.h>
 #import <SignalUI/ScreenLockViewController.h>
 #import <SignalUI/Theme.h>
-#import <SignalUI/ThreadViewHelper.h>
 #import <SignalUI/UIFont+OWS.h>
 #import <SignalUI/UIUtil.h>
 #import <SignalUI/UIView+SignalUI.h>

SignalUI/Views/ThreadViewHelper.h
@@ -1,30 +1,0 @@
-//
-//  Copyright (c) 2021 Open Whisper Systems. All rights reserved.
-//
-
-NS_ASSUME_NONNULL_BEGIN
-
-@protocol ThreadViewHelperDelegate <NSObject>
-
-- (void)threadListDidChange;
-
-@end
-
-#pragma mark -
-
-@class TSThread;
-
-// A helper class for views that want to present the list of threads
-// that show up in inbox, and in the same order.
-//
-// It observes changes to the threads & their ordering and informs
-// its delegate when they happen.
-@interface ThreadViewHelper : NSObject
-
-@property (nonatomic, weak) id<ThreadViewHelperDelegate> delegate;
-
-@property (nonatomic, readonly) NSMutableArray<TSThread *> *threads;
-
-@end
-
-NS_ASSUME_NONNULL_END

SignalUI/Views/ThreadViewHelper.m
@@ -1,150 +1,0 @@
-//
-//  Copyright (c) 2022 Open Whisper Systems. All rights reserved.
-//
-
-#import "ThreadViewHelper.h"
-#import <SignalServiceKit/AppContext.h>
-#import <SignalServiceKit/AppReadiness.h>
-#import <SignalServiceKit/SSKEnvironment.h>
-#import <SignalServiceKit/SignalServiceKit-Swift.h>
-#import <SignalServiceKit/StorageCoordinator.h>
-#import <SignalServiceKit/TSThread.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface ThreadViewHelper () <DatabaseChangeDelegate>
-
-@property (nonatomic) BOOL shouldObserveDBModifications;
-
-@end
-
-#pragma mark -
-
-@implementation ThreadViewHelper
-
-- (instancetype)init
-{
-    self = [super init];
-    if (!self) {
-        return self;
-    }
-
-    [self initializeMapping];
-
-    return self;
-}
-
-- (void)initializeMapping
-{
-    OWSAssertIsOnMainThread();
-
-    [self.databaseStorage appendDatabaseChangeDelegate:self];
-
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(applicationDidBecomeActive:)
-                                                 name:OWSApplicationDidBecomeActiveNotification
-                                               object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(applicationWillResignActive:)
-                                                 name:OWSApplicationWillResignActiveNotification
-                                               object:nil];
-
-    [self updateShouldObserveDBModifications];
-}
-
-- (void)applicationDidBecomeActive:(NSNotification *)notification
-{
-    [self updateShouldObserveDBModifications];
-}
-
-- (void)applicationWillResignActive:(NSNotification *)notification
-{
-    [self updateShouldObserveDBModifications];
-}
-
-- (void)updateShouldObserveDBModifications
-{
-    self.shouldObserveDBModifications = CurrentAppContext().isAppForegroundAndActive;
-}
-
-// Don't observe database change notifications when the app is in the background.
-//
-// Instead, rebuild model state when app enters foreground.
-- (void)setShouldObserveDBModifications:(BOOL)shouldObserveDBModifications
-{
-    if (_shouldObserveDBModifications == shouldObserveDBModifications) {
-        return;
-    }
-
-    _shouldObserveDBModifications = shouldObserveDBModifications;
-
-    if (shouldObserveDBModifications) {
-        [self updateThreads];
-    }
-}
-
-#pragma mark - Database
-
-- (void)updateThreads
-{
-    OWSAssertIsOnMainThread();
-
-    NSMutableArray<TSThread *> *threads = [NSMutableArray new];
-    [self.databaseStorage readWithBlock:^(SDSAnyReadTransaction *transaction) {
-        AnyThreadFinder *threadFinder = [AnyThreadFinder new];
-        NSError *_Nullable error;
-        [threadFinder enumerateVisibleThreadsWithIsArchived:NO
-                                                transaction:transaction
-                                                      error:&error
-                                                      block:^(TSThread *thread) { [threads addObject:thread]; }];
-        if (error != nil) {
-            OWSFailDebug(@"error: %@", error);
-        }
-    }];
-    _threads = [threads copy];
-}
-
-#pragma mark - DatabaseChangeDelegate
-
-- (void)databaseChangesDidUpdateWithDatabaseChanges:(id<DatabaseChanges>)databaseChanges
-{
-    OWSAssertIsOnMainThread();
-    OWSAssertDebug(AppReadiness.isAppReady);
-
-    if (![databaseChanges didUpdateModelWithCollection:TSThread.collection]) {
-        return;
-    }
-    if (!self.shouldObserveDBModifications) {
-        return;
-    }
-
-    [self updateThreads];
-}
-
-- (void)databaseChangesDidUpdateExternally
-{
-    OWSAssertIsOnMainThread();
-    OWSAssertDebug(AppReadiness.isAppReady);
-
-    if (!self.shouldObserveDBModifications) {
-        return;
-    }
-
-    [self updateThreads];
-}
-
-- (void)databaseChangesDidReset
-{
-    OWSAssertIsOnMainThread();
-    OWSAssertDebug(AppReadiness.isAppReady);
-
-    if (!self.shouldObserveDBModifications) {
-        return;
-    }
-
-    [self updateThreads];
-}
-
-@end
-
-NS_ASSUME_NONNULL_END

commit 174d42ec96f624a377843517fe19b3443d379e22

Fix a few bugs in AddToGroupViewController
- The `groupThreads` property was being written from a background thread
  and read from the main thread. It’s not atomic, though, so this isn’t
  safe. (This regressed in 622b5b2db11b8aa10bdf5ca3bc2eb9e6c6c0b844.)

- We were handling an impossible case related to GV1 groups. We filter
  out GV1 groups when fetching, so we can’t try to add to a GV1 group.

- We were handling an impossible case related to a missing UUID. At this
  point, everyone supports GV2 as long as they have a UUID. Since we
  already need to perform that check, show the “this person needs to
  update” error if we see there’s a missing UUID.

- We were assigning `navigationItem.title` instead of `title`.

- There was some extra whitespace.

Signal/src/ViewControllers/ThreadSettings/AddToGroupViewController.swift
@@ -30,7 +30,7 @@ public class AddToGroupViewController: OWSTableViewController2 {
     public override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationItem.title = NSLocalizedString("ADD_TO_GROUP_TITLE", comment: "Title of the 'add to group' view.")
+        title = NSLocalizedString("ADD_TO_GROUP_TITLE", comment: "Title of the 'add to group' view.")
 
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didPressCloseButton))
 
@@ -41,15 +41,17 @@ public class AddToGroupViewController: OWSTableViewController2 {
 
     private var groupThreads = [TSGroupThread]() {
         didSet {
-            DispatchQueue.main.async { [weak self] in
-                self?.updateTableContents()
-            }
+            AssertIsOnMainThread()
+            updateTableContents()
         }
     }
 
     private func updateGroupThreadsAsync() {
         DispatchQueue.sharedUserInitiated.async { [weak self] in
-            self?.groupThreads = Self.fetchGroupThreads()
+            let fetchedGroupThreads = Self.fetchGroupThreads()
+            DispatchQueue.main.async {
+                self?.groupThreads = fetchedGroupThreads
+            }
         }
     }
 
@@ -59,7 +61,7 @@ public class AddToGroupViewController: OWSTableViewController2 {
 
             do {
                 try AnyThreadFinder().enumerateGroupThreads(transaction: transaction) { thread in
-                    guard thread.groupModel.groupsVersion == .V2 else { return }
+                    guard thread.isGroupV2Thread else { return }
 
                     let threadViewModel = ThreadViewModel(
                         thread: thread,
@@ -127,35 +129,23 @@ public class AddToGroupViewController: OWSTableViewController2 {
             proceedTitle: NSLocalizedString("ADD_TO_GROUP_ACTION_PROCEED_BUTTON",
                                             comment: "The button on the 'add to group' confirmation to add the user to the group."),
             proceedStyle: .default) { _ in
-                self.addToGroupStep1(groupThread, shortName: shortName)
+                self.addToGroup(groupThread, shortName: shortName)
         }
     }
 
-    private func addToGroupStep1(_ groupThread: TSGroupThread, shortName: String) {
+    private func addToGroup(_ groupThread: TSGroupThread, shortName: String) {
         AssertIsOnMainThread()
-        guard groupThread.isGroupV2Thread else {
-            addToGroupStep2(groupThread, shortName: shortName)
-            return
-        }
-        let doesUserSupportGroupsV2 = GroupManager.doesUserSupportGroupsV2(address: self.address)
-        guard doesUserSupportGroupsV2 else {
+        owsAssert(groupThread.isGroupV2Thread)  // non-gv2 filtered above when fetching groups
+
+        guard let uuid = self.address.uuid else {
             GroupViewUtils.showInvalidGroupMemberAlert(fromViewController: self)
             return
         }
-        addToGroupStep2(groupThread, shortName: shortName)
-    }
 
-    private func addToGroupStep2(_ groupThread: TSGroupThread, shortName: String) {
         let oldGroupModel = groupThread.groupModel
 
-        guard !oldGroupModel.groupMembership.isMemberOfAnyKind(self.address) else {
+        guard !oldGroupModel.groupMembership.isMemberOfAnyKind(uuid) else {
             let error = OWSAssertionError("Receipient is already in group")
-            GroupViewUtils.showUpdateErrorUI(error: error)
-            return
-        }
-
-        guard let uuid = self.address.uuid else {
-            let error = OWSAssertionError("Address missing UUID")
             GroupViewUtils.showUpdateErrorUI(error: error)
             return
         }
@@ -189,7 +179,7 @@ public class AddToGroupViewController: OWSTableViewController2 {
 
     // MARK: -
 
-    private func item(forGroupThread  groupThread: TSGroupThread) -> OWSTableItem {
+    private func item(forGroupThread groupThread: TSGroupThread) -> OWSTableItem {
         let alreadyAMemberText = NSLocalizedString(
             "ADD_TO_GROUP_ALREADY_A_MEMBER",
             comment: "Text indicating your contact is already a member of the group on the 'add to group' view."