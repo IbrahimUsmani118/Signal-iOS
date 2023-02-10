IbrahimUsmani118/Signal-iOS

commit ec604e5359d67febaf8181b57ebd3db91b82445e

Add new registration nav controller and permissions screen (dormant)

Signal/src/ViewControllers/Registration/RegistrationPermissionsViewController.swift
@@ -1,0 +1,171 @@
+//
+// Copyright 2023 Signal Messenger, LLC
+// SPDX-License-Identifier: AGPL-3.0-only
+//
+
+import Foundation
+import Lottie
+
+// MARK: - RegistrationPermissionsState
+
+struct RegistrationPermissionsState {
+    let shouldRequestAccessToContacts: Bool
+}
+
+// MARK: - RegistrationPermissionsPresenter
+
+protocol RegistrationPermissionsPresenter: AnyObject {
+    func requestAccessToNotifications() -> Guarantee<Void>
+    func requestAccessToContacts() -> Guarantee<Void>
+    func goToNextStep()
+}
+
+// MARK: - RegistrationPermissionsViewController
+
+class RegistrationPermissionsViewController: OWSViewController {
+    private let state: RegistrationPermissionsState
+    private weak var presenter: RegistrationPermissionsPresenter?
+
+    public init(
+        state: RegistrationPermissionsState,
+        presenter: RegistrationPermissionsPresenter?
+    ) {
+        self.state = state
+        self.presenter = presenter
+
+        super.init()
+    }
+
+    public override init() {
+        owsFail("This should not be called")
+    }
+
+    // MARK: Rendering
+
+    private lazy var animationView: AnimationView = {
+        let animationView = AnimationView(name: "notificationPermission")
+        animationView.loopMode = .playOnce
+        animationView.backgroundBehavior = .pauseAndRestore
+        animationView.contentMode = .scaleAspectFit
+        return animationView
+    }()
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = Theme.backgroundColor
+
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.layoutMargins = UIEdgeInsets.layoutMarginsForRegistration(
+            traitCollection.horizontalSizeClass
+        )
+        stackView.isLayoutMarginsRelativeArrangement = true
+        view.addSubview(stackView)
+        stackView.autoPinEdgesToSuperviewMargins()
+
+        let titleText: String
+        let explanationText: String
+        let giveAccessText: String
+        if state.shouldRequestAccessToContacts {
+            titleText = OWSLocalizedString(
+                "ONBOARDING_PERMISSIONS_TITLE",
+                comment: "Title of the 'onboarding permissions' view."
+            )
+            explanationText = OWSLocalizedString(
+                "ONBOARDING_PERMISSIONS_EXPLANATION",
+                comment: "Explanation in the 'onboarding permissions' view."
+            )
+            giveAccessText = OWSLocalizedString(
+                "ONBOARDING_PERMISSIONS_ENABLE_PERMISSIONS_BUTTON",
+                comment: "Label for the 'give access' button in the 'onboarding permissions' view."
+            )
+        } else {
+            titleText = OWSLocalizedString(
+                "LINKED_ONBOARDING_PERMISSIONS_TITLE",
+                comment: "Title of the 'onboarding permissions' view."
+            )
+            explanationText = OWSLocalizedString(
+                "LINKED_ONBOARDING_PERMISSIONS_EXPLANATION",
+                comment: "Explanation in the 'onboarding permissions' view."
+            )
+            giveAccessText = OWSLocalizedString(
+                "LINKED_ONBOARDING_PERMISSIONS_ENABLE_PERMISSIONS_BUTTON",
+                comment: "Label for the 'give access' button in the 'onboarding permissions' view."
+            )
+        }
+
+        let titleLabel = UILabel.titleLabelForRegistration(text: titleText)
+        titleLabel.accessibilityIdentifier = "registration.permissions.titleLabel"
+        titleLabel.setCompressionResistanceVerticalHigh()
+        stackView.addArrangedSubview(titleLabel)
+        stackView.setCustomSpacing(20, after: titleLabel)
+
+        let explanationLabel = UILabel.explanationLabelForRegistration(text: explanationText)
+        explanationLabel.accessibilityIdentifier = "registration.permissions.explanationLabel"
+        explanationLabel.setCompressionResistanceVerticalHigh()
+        stackView.addArrangedSubview(explanationLabel)
+        stackView.setCustomSpacing(60, after: explanationLabel)
+
+        stackView.addArrangedSubview(animationView)
+        animationView.setContentHuggingHigh()
+
+        stackView.addArrangedSubview(UIView.vStretchingSpacer(minHeight: 80))
+
+        let giveAccessButton = OWSFlatButton.primaryButtonForRegistration(
+            title: giveAccessText,
+            target: self,
+            selector: #selector(giveAccessPressed)
+        )
+        giveAccessButton.accessibilityIdentifier = "registration.permissions.giveAccessButton"
+        stackView.addArrangedSubview(giveAccessButton)
+        giveAccessButton.autoSetDimension(.width, toSize: 280)
+        giveAccessButton.autoHCenterInSuperview()
+        NSLayoutConstraint.autoSetPriority(.defaultLow) {
+            giveAccessButton.autoPinEdge(toSuperviewEdge: .leading)
+            giveAccessButton.autoPinEdge(toSuperviewEdge: .trailing)
+        }
+    }
+
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        animationView.play()
+    }
+
+    // MARK: Events
+
+    @objc
+    private func giveAccessPressed() {
+        Logger.info("")
+
+        requestPermissions()
+    }
+
+    // MARK: Requesting permissions
+
+    private func requestPermissions() {
+        firstly { [weak self] () -> Guarantee<Void> in
+            guard let presenter = self?.presenter else { return .value(()) }
+
+            Logger.info("Requesting notification permissions")
+            return presenter.requestAccessToNotifications()
+        }.then { [weak self] () -> Guarantee<Void> in
+            guard let self, let presenter = self.presenter else {
+                return .value(())
+            }
+
+            if self.state.shouldRequestAccessToContacts {
+                Logger.info("Requesting contact permissions")
+                return presenter.requestAccessToContacts()
+            } else {
+                Logger.info("Not requesting contact permissions")
+                return .value(())
+            }
+        }.done { [weak self] in
+            Logger.info("Done requesting permissions")
+            self?.presenter?.completed()
+        }
+    }
+}

Signal.xcodeproj/project.pbxproj
@@ -1361,6 +1361,7 @@
 		F94D12FF28BD0DD900B2C478 /* SpeechManager.swift in Sources */ = {isa = PBXBuildFile; fileRef = F94D12FE28BD0DD900B2C478 /* SpeechManager.swift */; };
 		F94D130628C1667600B2C478 /* DatabaseRecoveryTest.swift in Sources */ = {isa = PBXBuildFile; fileRef = F94D130528C1667600B2C478 /* DatabaseRecoveryTest.swift */; };
 		F95427E6286E042200314EDA /* BadgeGiftingThanksSheet.swift in Sources */ = {isa = PBXBuildFile; fileRef = F95427E5286E042200314EDA /* BadgeGiftingThanksSheet.swift */; };
+		F95A64F3299589CA007FDBDF /* RegistrationNavigationController.swift in Sources */ = {isa = PBXBuildFile; fileRef = F95A64F2299589CA007FDBDF /* RegistrationNavigationController.swift */; };
 		F95D71A3299305C400ED3102 /* RegistrationViewUtil.swift in Sources */ = {isa = PBXBuildFile; fileRef = F95D71A2299305C400ED3102 /* RegistrationViewUtil.swift */; };
 		F9613CDC2981F11400894B55 /* SqliteUtil.swift in Sources */ = {isa = PBXBuildFile; fileRef = F9613CDB2981F11400894B55 /* SqliteUtil.swift */; };
 		F9613CDE2981F15700894B55 /* SqliteUtilTest.swift in Sources */ = {isa = PBXBuildFile; fileRef = F9613CDD2981F15700894B55 /* SqliteUtilTest.swift */; };
@@ -2153,6 +2154,7 @@
 		F9D5BFCD2979A017001737E5 /* OWSRequestFactory+Spam.swift in Sources */ = {isa = PBXBuildFile; fileRef = F9D5BFCC2979A017001737E5 /* OWSRequestFactory+Spam.swift */; };
 		F9D5BFCF2979AFF4001737E5 /* URLPathComponents.swift in Sources */ = {isa = PBXBuildFile; fileRef = F9D5BFCE2979AFF4001737E5 /* URLPathComponents.swift */; };
 		F9D5BFD12979B027001737E5 /* URLPathComponentsTest.swift in Sources */ = {isa = PBXBuildFile; fileRef = F9D5BFD02979B027001737E5 /* URLPathComponentsTest.swift */; };
+		F9D5C39F2993F9FF004891FC /* RegistrationPermissionsViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = F9D5C39E2993F9FF004891FC /* RegistrationPermissionsViewController.swift */; };
 		F9D83012282DBB1500399363 /* BadgeGiftingChooseBadgeViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = F9D83011282DBB1500399363 /* BadgeGiftingChooseBadgeViewController.swift */; };
 		F9DD70B92811AF82000C5960 /* DonationViewsUtil.swift in Sources */ = {isa = PBXBuildFile; fileRef = F9DD70B82811AF82000C5960 /* DonationViewsUtil.swift */; };
 		F9E3720C2948E32800975224 /* BadgeGiftingConfirmationViewController+ApplePay.swift in Sources */ = {isa = PBXBuildFile; fileRef = F9E3720B2948E32800975224 /* BadgeGiftingConfirmationViewController+ApplePay.swift */; };
@@ -3848,6 +3850,7 @@
 		F94D12FE28BD0DD900B2C478 /* SpeechManager.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SpeechManager.swift; sourceTree = "<group>"; };
 		F94D130528C1667600B2C478 /* DatabaseRecoveryTest.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = DatabaseRecoveryTest.swift; sourceTree = "<group>"; };
 		F95427E5286E042200314EDA /* BadgeGiftingThanksSheet.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BadgeGiftingThanksSheet.swift; sourceTree = "<group>"; };
+		F95A64F2299589CA007FDBDF /* RegistrationNavigationController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = RegistrationNavigationController.swift; sourceTree = "<group>"; };
 		F95D71A2299305C400ED3102 /* RegistrationViewUtil.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = RegistrationViewUtil.swift; sourceTree = "<group>"; };
 		F9613CDB2981F11400894B55 /* SqliteUtil.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SqliteUtil.swift; sourceTree = "<group>"; };
 		F9613CDD2981F15700894B55 /* SqliteUtilTest.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = SqliteUtilTest.swift; sourceTree = "<group>"; };
@@ -4641,6 +4644,7 @@
 		F9D5BFCC2979A017001737E5 /* OWSRequestFactory+Spam.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "OWSRequestFactory+Spam.swift"; sourceTree = "<group>"; };
 		F9D5BFCE2979AFF4001737E5 /* URLPathComponents.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = URLPathComponents.swift; sourceTree = "<group>"; };
 		F9D5BFD02979B027001737E5 /* URLPathComponentsTest.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = URLPathComponentsTest.swift; sourceTree = "<group>"; };
+		F9D5C39E2993F9FF004891FC /* RegistrationPermissionsViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = RegistrationPermissionsViewController.swift; sourceTree = "<group>"; };
 		F9D83011282DBB1500399363 /* BadgeGiftingChooseBadgeViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BadgeGiftingChooseBadgeViewController.swift; sourceTree = "<group>"; };
 		F9DD70B82811AF82000C5960 /* DonationViewsUtil.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = DonationViewsUtil.swift; sourceTree = "<group>"; };
 		F9E3720B2948E32800975224 /* BadgeGiftingConfirmationViewController+ApplePay.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "BadgeGiftingConfirmationViewController+ApplePay.swift"; sourceTree = "<group>"; };
@@ -4910,6 +4914,8 @@
 				34ACA7ED2733161000E47AD4 /* Deprecated_RegistrationHelper.swift */,
 				34404274273C0E41008C9FA4 /* Deprecated_RegistrationPinAttemptsExhaustedViewController.swift */,
 				34ACA7EC2733161000E47AD4 /* Deprecated_RegistrationVerificationViewController.swift */,
+				F95A64F2299589CA007FDBDF /* RegistrationNavigationController.swift */,
+				F9D5C39E2993F9FF004891FC /* RegistrationPermissionsViewController.swift */,
 				F933FC3E2992E77300D78DB0 /* RegistrationSplashViewController.swift */,
 				F95D71A2299305C400ED3102 /* RegistrationViewUtil.swift */,
 			);
@@ -11086,6 +11092,8 @@
 				88863A56280CD04B00977F69 /* ReactionFlybyAnimation.swift in Sources */,
 				88B688B0238F0D1000286F82 /* ReactionsDetailSheet.swift in Sources */,
 				8809CE8A22F93C2200D38867 /* RecentPhotoCollectionView.swift in Sources */,
+				F95A64F3299589CA007FDBDF /* RegistrationNavigationController.swift in Sources */,
+				F9D5C39F2993F9FF004891FC /* RegistrationPermissionsViewController.swift in Sources */,
 				F933FC3F2992E77300D78DB0 /* RegistrationSplashViewController.swift in Sources */,
 				34E5DC8220D8050D00C08145 /* RegistrationUtils.m in Sources */,
 				3440426B273AB186008C9FA4 /* RegistrationUtils.swift in Sources */,

Signal/src/ViewControllers/Registration/RegistrationNavigationController.swift
@@ -1,0 +1,15 @@
+//
+// Copyright 2023 Signal Messenger, LLC
+// SPDX-License-Identifier: AGPL-3.0-only
+//
+
+import Foundation
+
+public class RegistrationNavigationController: OWSNavigationController {
+    public override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        let superOrientations = super.supportedInterfaceOrientations
+        let onboardingOrientations: UIInterfaceOrientationMask = UIDevice.current.isIPad ? .all : .portrait
+
+        return superOrientations.intersection(onboardingOrientations)
+    }
+}

Signal/src/ViewControllers/Registration/RegistrationViewUtil.swift
@@ -36,6 +36,17 @@ extension UILabel {
         result.textAlignment = .center
         return result
     }
+
+    static func explanationLabelForRegistration(text: String) -> UILabel {
+        let result = UILabel()
+        result.textColor = Theme.secondaryTextAndIconColor
+        result.font = UIFont.ows_dynamicTypeSubheadlineClamped
+        result.text = text
+        result.numberOfLines = 0
+        result.textAlignment = .center
+        result.lineBreakMode = .byWordWrapping
+        return result
+    }
 }
 
 // MARK: - Buttons

Signal/src/environment/SignalApp.swift
@@ -74,7 +74,7 @@ extension SignalApp {
 
     private func showRegistrationView(appDelegate: UIApplicationDelegate) {
         // This code is likely to change significantly.
-        let navController = OWSNavigationController()
+        let navController = RegistrationNavigationController()
         navController.setViewControllers([RegistrationSplashViewController()], animated: false)
 
         appDelegate.window??.rootViewController = navController

commit 61168be19c64a2f42b319a37df59fc2dfdcc6b05

Fix incorrect function name causing compile failure
I messed this up in ec604e5359d67febaf8181b57ebd3db91b82445e which
caused compilation failures.

Signal/src/ViewControllers/Registration/RegistrationPermissionsViewController.swift
@@ -165,7 +165,7 @@ class RegistrationPermissionsViewController: OWSViewController {
             }
         }.done { [weak self] in
             Logger.info("Done requesting permissions")
-            self?.presenter?.completed()
+            self?.presenter?.goToNextStep()
         }
     }
 }